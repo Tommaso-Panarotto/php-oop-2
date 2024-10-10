bbbdf2bc45db717f554d40fa66959645a9991027
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-10 15:30:16 +0200

    food class

---
+++ classes/Foods.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/Product.php";
+
+class Foods extends Product
+{
+
+    //instance variables
+    public $consistence; //solid or liquid
+    public $expiration; //expiration date
+
+    //constructor
+    public function __construct(string $_name, int $_price, string $_imageUrl, string $_consistence, string $_expiration)
+    {
+        //call constructor of the parent
+        parent::__construct("Food", $_name, $_price, $_imageUrl);
+        $this->consistence = $_consistence;
+        $this->expiration = $_expiration;
+    }
+}
